6abdb677fb6b1407deb58a9c8e78a980dd565196
Joseph Lee <jhjlee702@gmail.com>
2023-11-11 16:03:09 -0700

    small change

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,6 +1,7 @@
 class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+  has_many :ingredients, through: :dishes
 
   def uniq_ingredients
     Ingredient.distinct.pluck(:name)
